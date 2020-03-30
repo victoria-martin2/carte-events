b64de20aa500b070a06acd4628f09b831a5f7f62
victoria-martin2 <victoria-c.martin@hotmail.fr>
2020-03-30 18:08:38 +0200

    change

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ var mymap = L.map('mapid').setView([48.8534, 2.3488], 13);
 //     [51.51, -0.047]
 // ]).addTo(mymap);
 
+var layerGroup = L.layerGroup().addTo(mymap);
+
+
 L.tileLayer(
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
     {
@@ -33,9 +36,15 @@ L.tileLayer(
 ).addTo(mymap);
 
 async function getData(query) {
-    let url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=date_start+%3E%3D+%23now()+AND+date_start+%3C+%23now(months%3D1)&rows=50&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type"
+    if(query == undefined) {
+        query = "";
+    }
+    let url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=date_start+%3E%3D+%23now()+AND+date_start+%3C+%23now(months%3D1) " + searchInput.value + "&rows=100&facet=category&facet=tags&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type"
     let response = await fetch(url)
     let data = await response.json()
+
+    layerGroup.clearLayers();
+
     data.records.forEach(function(event) {
         // le titre de l'événement
         let title = event.fields.title
@@ -52,15 +61,11 @@ async function getData(query) {
 
         var marker = L.marker([latitude, longitude]).addTo(mymap);
 
-        // afficher une pop up avec la fonction bindPopup
-        // ...
-
-        // ajouter à ma carte 
-        marker.addTo(mymap);
-
-
         // .. mais ce serait mieux de les afficher sur la carte !
-        marker.bindPopup("<strong>" + event.fields.title + "</strong>" + "<br>"+ event.fields.address_street).openPopup();
+        marker.bindPopup("<strong>" + event.fields.title + "</strong>" + "<br>"+ event.fields.address_street + "<div class='description'>" + event.fields.description + "</div>").openPopup();
+
+        marker.addTo(layerGroup);
+
     })
 }
 getData()
@@ -69,10 +74,8 @@ function onFormSubmit (event){
     event.preventDefault();
     console.log("le formulaire marche");
     console.log(searchInput.value);
-
-    //getData(searchInput.value)
+    getData(searchInput.value)
 }
-
 
 
 
